df6b9853b5d82d0e075736a9a802910e221e2e39
Nebula <me@itsnebula.net>
2023-02-03 18:29:39 -0500

    config: disable ssr for now

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,12 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-import cloudflare from "@astrojs/cloudflare";
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    config: {
-      applyBaseStyles: false
-    }
-  })],
-  output: "server",
-  adapter: cloudflare()
+    integrations: [
+        tailwind({
+            config: {
+                applyBaseStyles: false,
+            },
+        }),
+    ],
 });
