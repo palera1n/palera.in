d285427db6116a37fc24fe6dc5b5cd60b5bc0963
Nebula <me@itsnebula.net>
2023-02-03 09:52:19 -0500

    config: set up cloudflare ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,15 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import cloudflare from "@astrojs/cloudflare";
+
+// https://astro.build/config
 export default defineConfig({
-    integrations: [
-        tailwind({
-            config: { applyBaseStyles: false },
-        }),
-    ],
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  })],
+  output: "server",
+  adapter: cloudflare()
 });
