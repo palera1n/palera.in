2758075a4006e6c01acfdf241ea1ba2b2435dc85
flower <97859147+flowerible@users.noreply.github.com>
2023-05-23 18:01:24 -0700

    feat: More credits and credits page for website

--- src/pages/credits.json.ts
+++ src/pages/credits.json.ts
@@ -1,5 +1,13 @@
 import credits from "../lib/credits.json";
+import thanks from "../lib/credits.json";
+import translations from "../lib/credits.json";
 
 export async function get({ params, request }) {
-    return {body: JSON.stringify(credits)};
+    const responses = [
+        { body: JSON.stringify(credits) },
+        { body: JSON.stringify(thanks) },
+        { body: JSON.stringify(translations) }
+    ];
+
+    return responses;
 }
