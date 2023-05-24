81d394b15a2023b42b29db9672df176cd2456b45
Nebula <me@itsnebula.net>
2023-05-23 21:08:52 -0400

    fix(credits): json should only be imported once

--- src/pages/credits.json.ts
+++ src/pages/credits.json.ts
@@ -1,13 +1,5 @@
 import credits from "../lib/credits.json";
-import thanks from "../lib/credits.json";
-import translations from "../lib/credits.json";
 
 export async function get({ params, request }) {
-    const responses = [
-        { body: JSON.stringify(credits) },
-        { body: JSON.stringify(thanks) },
-        { body: JSON.stringify(translations) }
-    ];
-
-    return responses;
+    return { body: JSON.stringify(credits) };
 }
