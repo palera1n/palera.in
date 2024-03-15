e37237dbb77d63ad0b432f15858793d66de13e59
Samara <97859147+ssalggnikool@users.noreply.github.com>
2024-03-14 19:45:36 -0700

    update footer and links

--- src/scripts/constants.ts
+++ src/scripts/constants.ts
@@ -5,7 +5,9 @@ export const APPL_SOURCE_CODE = "https://github.com/palera1n/loader";
 export const MAIN_GITHUB_LINK = "https://github.com/palera1n";
 export const MAIN_DISCOR_LINK = "https://dsc.gg/palera1n";
 export const MAIN_TWITTE_LINK = "https://twitter.com/palera1n";
+export const MAIN_MASTOD_LINK = "https://procursus.social/@palera1n";
 export const MAIN_SOURCE_PUBL = "https://github.com/palera1n/palera1n/releases";
+export const MAIN_REPO_SOURCE = "https://repo.palera.in";
 export const SISTER_TOOL_PUBL = "https://kok3shidoll.web.app/bakera1n.html";
 
 export const IS_SERVER = import.meta.env.SSR;
